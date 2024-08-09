321fa3ea46317a3de80cd13a36664659108d10cf
Tamme Dittrich <tamme@tweedegolf.com>
2024-08-09 14:57:35 +0200

    Create frontend that looks more like crates.io

--- frontend/src/lib/CrateList.svelte
+++ frontend/src/lib/CrateList.svelte
@@ -1,72 +1,27 @@
 <script lang="ts">
-    import type {Crate} from "../crate-db";
+    import type {FullCrate} from "../crate-db";
+    import Crate from "./Crate.svelte";
 
-    export let crates: Crate[];
+    export let crates: FullCrate[];
 
     export let filter: number[];
     export let cols_shown: string[] = ["name", "description", "downloads", "updated_at"];
 
-    let filtered_crates: Crate[] = crates;
+    let filtered_crates: FullCrate[] = crates;
 </script>
 
-<table>
-    <thead>
-    <tr>
-        <th>Crate</th>
-        {#if cols_shown.includes("description")}
-            <th>Description</th>
-        {/if}
-        {#if cols_shown.includes("interfaces")}
-            <th>Interfaces</th>
-        {/if}
-        {#if cols_shown.includes("license")}
-            <th>License</th>
-        {/if}
-        {#if cols_shown.includes("downloads")}
-            <th>Downloads</th>
-        {/if}
-        {#if cols_shown.includes("updated_at")}
-            <th>Last Updated</th>
-        {/if}
-        {#if cols_shown.includes("created_at")}
-            <th>Created At</th>
-        {/if}
-        {#if cols_shown.includes("rust_version")}
-            <th>Rust Version</th>
-        {/if}
-    </tr>
-    </thead>
-    <tbody>
-    {#each filtered_crates as crate, i}
-        {#if filter.includes(i)}
-            <tr>
-                <td><a href="https://crates.io/crates/{crate.name}">{crate.name}
-                    {#if cols_shown.includes("version")}@ {crate.version}{/if}
-                </a></td>
-                {#if cols_shown.includes("description")}
-                    <td>{crate.description}</td>
-                {/if}
-                {#if cols_shown.includes("interfaces")}
-                    <td>{crate.interfaces.join(", ")}</td>
-                {/if}
-                {#if cols_shown.includes("license")}
-                    <td>{crate.license}</td>
-                {/if}
-                {#if cols_shown.includes("downloads")}
-                    <td class="right">{crate.downloads}</td>
-                {/if}
-                {#if cols_shown.includes("updated_at")}
-                    <td class="right">{new Date(crate.updated_at).toLocaleDateString()}</td>
-                {/if}
-                {#if cols_shown.includes("created_at")}
-                    <td class="right">{new Date(crate.created_at).toLocaleDateString()}</td>
-                {/if}
-                {#if cols_shown.includes("rust_version")}
-                    <td class="right">{crate.rust_version || ""}</td>
-                {/if}
-            </tr>
-        {/if}
 
-    {/each}
-    </tbody>
-</table>
+<ol>
+{#each filtered_crates as crate, i}
+    {#if filter.includes(i)}
+        <li><Crate crate={crate}/></li>
+    {/if}
+{/each}
+</ol>
+
+<style>
+    ol {
+        list-style-type: none;
+        padding: 0;
+    }
+</style>
