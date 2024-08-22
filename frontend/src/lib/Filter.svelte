1bc3ce395ea389de7dea1792e02756b60ad78479
Tamme Dittrich <tamme@tweedegolf.com>
2024-08-22 18:24:32 +0200

    Fix svelte-check warnings

--- frontend/src/lib/Filter.svelte
+++ frontend/src/lib/Filter.svelte
@@ -7,8 +7,8 @@
     export let selected: number[][];
 
     enum SortType {
-      Name,
-      Count,
+        Name,
+        Count,
     }
 
     let open: boolean = false;
@@ -16,41 +16,38 @@
     let sort_by: SortType = SortType.Name;
     let sort_direction: boolean = true;
 
-    function sort(items, sort_by: SortType, sort_direction: boolean) {
+    function sort(items: { [key: string]: number[] }, sort_by: SortType, sort_direction: boolean) {
+        let checked = Object.entries(items).filter((i) => selected.includes(i[1]));
+        let unchecked = Object.entries(items).filter((i) => !selected.includes(i[1]));
 
-      let checked = Object.entries(items).filter((i) => selected.includes(i[1]));
-      let unchecked = Object.entries(items).filter((i) => !selected.includes(i[1]));
+        if (sort_by == SortType.Count) {
+            checked = checked.sort((a, b) => b[1].length - a[1].length);
+            unchecked = unchecked.sort((a, b) => b[1].length - a[1].length);
+        }
 
-      if (sort_by == SortType.Count) {
-        checked = checked.sort((a, b) => b[1].length - a[1].length);
-        unchecked = unchecked.sort((a, b) => b[1].length - a[1].length);
-      }
+        if (!sort_direction) {
+            checked = checked.reverse();
+            unchecked = unchecked.reverse();
+        }
 
-      if (!sort_direction) {
-        checked = checked.reverse();
-        unchecked = unchecked.reverse();
-      }
-
-      let results = checked.concat(unchecked);
-
-      return results;
+        return checked.concat(unchecked);
     }
 
     function update_sort(type: SortType) {
-      if (sort_by == type) {
-        sort_direction = !sort_direction;
-      } else {
-        sort_by = type;
-        sort_direction = true;
-      }
+        if (sort_by == type) {
+            sort_direction = !sort_direction;
+        } else {
+            sort_by = type;
+            sort_direction = true;
+        }
     }
 
     function select_all() {
-      selected = [...Object.values(values)];
+        selected = [...Object.values(values)];
     }
 
     function select_none() {
-      selected = [];
+        selected = [];
     }
 
     $: sorted_values = sort(values, sort_by, sort_direction);
@@ -59,33 +56,37 @@
 </script>
 
 <div class={open ? 'filter open' : 'filter'}>
-  <div class="filter-box" on:click={() => $open_filter == name ? $open_filter = "" : $open_filter = name}>
-    <span class="filter-name">{name}</span>
-    {#if count> 0}<span class="filter-count">{count}</span>{/if}
-    <span class="filter-wedge">❮</span>
-  </div>
-  <div class={open ? 'filter-popup' : 'filter-popup hidden'}>
-    <div class="filter-top">
-      <div class={ sort_by == SortType.Name ? sort_direction ? "sort-item sorted asc" : 'sort-item sorted desc' : "sort-item"} on:click={() => update_sort(SortType.Name)}>Name</div>
-      <div class={ sort_by == SortType.Count ? sort_direction ? "sort-item sorted asc" : 'sort-item sorted desc' : "sort-item"} on:click={() => update_sort(SortType.Count)}>Count</div>
+    <button class="filter-box" on:click={() => $open_filter === name ? $open_filter = "" : $open_filter = name}>
+        <span class="filter-name">{name}</span>
+        {#if count > 0}<span class="filter-count">{count}</span>{/if}
+        <span class="filter-wedge">❮</span>
+    </button>
+    <div class={open ? 'filter-popup' : 'filter-popup hidden'}>
+        <div class="filter-top">
+            <button class={ sort_by === SortType.Name ? sort_direction ? "sort-item sorted asc" : 'sort-item sorted desc' : "sort-item"}
+                    on:click={() => update_sort(SortType.Name)}>Name
+            </button>
+            <button class={ sort_by === SortType.Count ? sort_direction ? "sort-item sorted asc" : 'sort-item sorted desc' : "sort-item"}
+                    on:click={() => update_sort(SortType.Count)}>Count
+            </button>
 
-      <button on:click={select_all}>All</button>
-      <button on:click={select_none}>None</button>
-    </div>
+            <button on:click={select_all}>All</button>
+            <button on:click={select_none}>None</button>
+        </div>
 
-    <div class="filter-list">
-      {#each sorted_values as [key, value]}
-          {#if key.toLowerCase().includes(filter.toLowerCase())}
-              <label>
-                  <input type="checkbox" bind:group="{selected}" value={value}>
-                  {key} ({value.length})
-              </label>
-           {/if}
-      {/each}
+        <div class="filter-list">
+            {#each sorted_values as [key, value]}
+                {#if key.toLowerCase().includes(filter.toLowerCase())}
+                    <label>
+                        <input type="checkbox" bind:group="{selected}" value={value}>
+                        {key} ({value.length})
+                    </label>
+                {/if}
+            {/each}
+        </div>
+        <div class="filter-search">
+            🔍 <input type="text" bind:value={filter}/>
+            <button on:click={() => filter = ""}>Clear</button>
+        </div>
     </div>
-    <div class="filter-search">
-      🔍 <input type="text" bind:value={filter} />
-      <button on:click={() => filter = ""}>Clear</button>
-    </div>
-  </div>
 </div>
