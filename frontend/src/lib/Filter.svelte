d800b90d3ad9bbd16683b8545cc4bd57a7385255
Bram Tweedegolf <bram@tweedegolf.com>
2024-09-17 09:20:03 +0200

    Improved filters slightly

--- frontend/src/lib/Filter.svelte
+++ frontend/src/lib/Filter.svelte
@@ -16,6 +16,30 @@
     let sort_by: SortType = SortType.Name;
     let sort_direction: boolean = true;
 
+    function clickOutside(node, { enabled: initialEnabled, cb }) {
+        const handleOutsideClick = ({ target }) => {
+          if (!node.contains(target)) {
+            cb();
+          }
+        };
+
+        function update({enabled}) {
+          if (enabled) {
+            window.addEventListener('click', handleOutsideClick);
+          } else {
+            window.removeEventListener('click', handleOutsideClick);
+          }
+        }
+
+        update({ enabled: initialEnabled });
+        return {
+          update,
+          destroy() {
+            window.removeEventListener( 'click', handleOutsideClick );
+          }
+        };
+    }
+
     function sort(items: { [key: string]: number[] }, sort_by: SortType, sort_direction: boolean) {
         let checked = Object.entries(items).filter((i) => selected.includes(i[1]));
         let unchecked = Object.entries(items).filter((i) => !selected.includes(i[1]));
@@ -55,24 +79,21 @@
     $: open = $open_filter == name;
 </script>
 
-<div class={open ? 'filter open' : 'filter'}>
+<div class={open ? 'filter open' : 'filter'} use:clickOutside={{ enabled: open, cb: () => open = false }} >
     <button class="filter-box" on:click={() => $open_filter === name ? $open_filter = "" : $open_filter = name}>
         <span class="filter-name">{name}</span>
         {#if count > 0}<span class="filter-count">{count}</span>{/if}
         <span class="filter-wedge">❮</span>
     </button>
     <div class={open ? 'filter-popup' : 'filter-popup hidden'}>
-        <div class="filter-top">
-            <button class={ sort_by === SortType.Name ? sort_direction ? "sort-item sorted asc" : 'sort-item sorted desc' : "sort-item"}
-                    on:click={() => update_sort(SortType.Name)}>Name
-            </button>
-            <button class={ sort_by === SortType.Count ? sort_direction ? "sort-item sorted asc" : 'sort-item sorted desc' : "sort-item"}
-                    on:click={() => update_sort(SortType.Count)}>Count
-            </button>
 
-            <button on:click={select_all}>All</button>
-            <button on:click={select_none}>None</button>
+        {#if Object.entries(values).length > 10}
+        <div class="filter-search">
+            🔍 <input type="text" bind:value={filter} placeholder="Search { name }" />
+            <!--<button on:click={() => filter = ""}>𐄂</button>-->
         </div>
+        {/if}
+
 
         <div class="filter-list">
             {#each sorted_values as [key, value]}
@@ -84,9 +105,22 @@
                 {/if}
             {/each}
         </div>
-        <div class="filter-search">
-            🔍 <input type="text" bind:value={filter}/>
-            <button on:click={() => filter = ""}>Clear</button>
+
+        {#if Object.entries(values).length > 5}
+        <div class="filter-bottom">
+            Sort by
+            <button class={ sort_by === SortType.Name ? sort_direction ? "sort-item sorted asc" : 'sort-item sorted desc' : "sort-item"}
+                    on:click={() => update_sort(SortType.Name)}>Name
+            </button>
+            <button class={ sort_by === SortType.Count ? sort_direction ? "sort-item sorted asc" : 'sort-item sorted desc' : "sort-item"}
+                    on:click={() => update_sort(SortType.Count)}>Count
+            </button>
+
+            Select
+            <button on:click={select_all}>🗹</button>
+            <button on:click={select_none}>☐</button>
         </div>
+        {/if}
+
     </div>
 </div>
