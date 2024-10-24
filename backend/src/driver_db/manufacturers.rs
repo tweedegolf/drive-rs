50f17cf1ef430bafb195c2b93bd13c649b827632
Tamme Dittrich <tamme@tweedegolf.com>
2024-10-24 18:14:28 +0200

    Rework categories and boards

--- backend/src/driver_db/manufacturers.rs
+++ backend/src/driver_db/manufacturers.rs
@@ -2,11 +2,14 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, PartialEq, Default, Serialize, Deserialize, JsonSchema)]
-pub enum Manufacturer {
+pub enum ChipManufacturer {
     AnalogDevices,
+    /// Also known as ST Micro
     ST,
     TI,
     NXP,
+    Toshiba,
+    SolomonSystech,
     #[default]
     Unknown,
 }
